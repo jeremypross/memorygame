5f3887fa3ad924f1e861d1f67d1a6b0555dcc449
Jeremy Ross <jeremypross@gmail.com>
2017-01-27 15:29:50 -0500

    click event progress

--- js/main.js
+++ js/main.js
@@ -5,79 +5,103 @@ document.addEventListener('DOMContentLoaded', function(){
   // create deck with 8 matching pairs
   var cardValues = ['2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9'];
 
-// function to randomize card order
-// credit: fisher yates shuffle (Source #2 in readme.md) (http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array) & (https://bost.ocks.org/mike/shuffle/)
+  var counter = 0;
 
+  var score = 0;
+
+  // function to randomize card order
+  // credit: fisher yates shuffle (Source #2 in readme.md) (http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array) & (https://bost.ocks.org/mike/shuffle/)
   var shuffle = function(arr) {
     var currentIndex = arr.length, temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-      // Pick a remaining array element...
+      // Pick a remaining array element:
       // makes random number based on the array length
       // will find random number between 0 and length
       randomIndex = Math.floor(Math.random() * currentIndex);
       // works way down through array, from last value until it reaches 0
-      // minuses current index by 1
+      // works way down through array by decreasing current index by 1
       currentIndex -= 1;
-
-      // And swap it with the current element.
-      // eliminates need for push(); and pop();
+      // And swap it with the current element (eliminates need for push(); and pop();)
       temporaryValue = arr[currentIndex];
       arr[currentIndex] = arr[randomIndex];
       arr[randomIndex] = temporaryValue;
     }
     return arr;
   }
+  // checking work
   console.log(shuffle(cardValues));
 
-
-
-// function - iterate through the array, and then give each div the value of the the array value.
+  // function - iterate through the array, and then give each div the value of the the array value.
   var createDeck = function(){
     cardValues.forEach(function(card){
       // create empty string
       var newText = '';
       // change value of empty string
-      newText += '<div class=\'col\'>' +
-                 '<div class=\'card-container\'>' +
-                 '<div class=\'card\'>' +
-                 '<div class=\'face front\'>' +
-                 '<div class=\'frontside\'>' +
-                 '<img src=\'img/cardback.jpg\' />' +
-                 '</div>' +
-                 '</div>' +
-                 '<div class=\'face back\'>' +
-                 //  adding array value here
-                 '<div class=\'cardvalue\'>' + card + '</div>' +
-                 '</div>'+'</div>'+'</div>'+'</div>';
+      newText += '<div class=\'col\'>'+'<div class=\'card-container\'>'+'<div class=\'card\'>'+'<div class=\'face front\'>'+'<img src =\'img/cardback.jpg\' />' + '</div>' +'<div class=\'face back\'>'+'<div class=\'cardvalue\'>'+ card +'</div>'+'</div>'+'</div>'+'</div>'+'</div>';
       // check work
       console.log(newText);
       // create a variable for container element
       var container = document.getElementById('container');
       // place array of strings into container element
-        // use += operator to add each string in loop - not just last one
+      // use += operator to add each string in loop - not just last one
       container.innerHTML += newText;
     });
   };
   createDeck();
 
 
+  // create variable for front of card - for click event
+  var frontOfCard = document.querySelectorAll('.face.front');
 
-// SECOND
-// 1. write function to start game - which has series of conditional statements
-// figure out how to keep track of clicks
-// after two clicks if it does'nt match
-// figure out how to reset it
-// think ahead
+  frontOfCard.forEach(function(card) {
+    card.addEventListener('click', function() {
+    if (counter <=1){
+      // create variable for back of card
+      var backOfCard = document.querySelectorAll('.face.back');
+      console.log('clicked');
+      console.log(card.nextSibling);
+      // on click - toggle class back
+      card.nextSibling.classList.toggle('back');
 
-// 1. html / game board
-// 2. next is game logic
-// design in a way to create business logic
-// THIRD IS WORRYING ABOUT STYLING
+      var frontImage = document.querySelector('img');
+      frontImage.style.display = 'none';
+      // on click - toggle class front
+      card.classList.toggle('front');
+      // increase counter by 1
+      counter +=1;
+      console.log(counter);
 
+      // push values to compare into new array
+      // compare the first and second index values from that array
+    } else {
+      alert('Please choose another card!');
+    };
+  });
+
+
+
+    // need dual click event
+
+    // if the counter is equal to 2
+      // conditional statement to evaluate matches
+        // if the cards match - then increase score by 1 and alert user
+        // else flip cards back over
+          // toggle class front
+    // else
+      // ask user to choose another card
+
+  });
+
+
+  // PART II
+  // 1. write function to start game - which has series of conditional statements
+  // figure out how to keep track of clicks
   // check if match in click event, call back function to check if those are equal
-  // then you need a mechanism to check if the two choices match
+  // figure out how to reset game
+
+
 
 
 });
